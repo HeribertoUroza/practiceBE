3a7e4ab676409ff589b20b3c56c4b5ed3db5c8ab
Heriberto Uroza <heribertouroza@pursuit.org>
2020-05-05 09:20:21 -0400

    renamed routes to fit description. currently working, one more api to add

--- app.py
+++ app.py
@@ -3,8 +3,18 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/ping')
-def test():
+@app.route('/cocktail')
+def cocktail():
+    url = 'https://the-cocktail-db.p.rapidapi.com/random.php'
+    headers = {
+        "x-rapidapi-host": 'the-cocktail-db.p.rapidapi.com',
+        "x-rapidapi-key": ''
+    }
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+@app.route('/weather')
+def weather():
     weatherAPI = ''
     url = 'https://api.darksky.net/forecast/' + weatherAPI + '/40.7829,73.9654'
     
