9baa8d37248723a3e0208d9a558bfb7c0e7dc865
Heriberto Uroza <heribertouroza@pursuit.org>
2020-05-15 15:06:23 -0400

    added enviroment vars, testing now

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request
 from flask_cors import CORS
+import os
 import requests
 import json
 
 app = Flask(__name__)
 cors = CORS(app)
+conn = boto.connection_s3()
 
 @app.route('/cocktail')
 def cocktail():
-    cocktailAPI = ''
+    cocktailAPI = os.environ['cocktailAPI']
     url = 'https://the-cocktail-db.p.rapidapi.com/random.php'
     headers = {
         "x-rapidapi-host": 'the-cocktail-db.p.rapidapi.com',
@@ -21,15 +23,15 @@ def cocktail():
 @app.route('/weather', methods=['GET'])
 def weather():
     if request.method == 'GET':
-        weatherAPI = ''
-    url = 'https://api.darksky.net/forecast/' + weatherAPI + '/40.7829,73.9654'
+        weatherAPI = os.environ['weatherAPI']
+        url = 'https://api.darksky.net/forecast/' + weatherAPI + '/40.7829,73.9654'
     
-    r = requests.get(url)
-    return r.json()
+        r = requests.get(url)
+        return r.json()
 
 @app.route('/nasa')
 def nasa():
-    nasaAPI = ''
+    nasaAPI = os.environ['nasaAPI']
     url = 'https://api.nasa.gov/planetary/apod?api_key=' + nasaAPI
 
     r = requests.get(url)
@@ -41,7 +43,7 @@ def gif():
         query = json.loads(request.data.decode('UTF-8'))
 
         params = {
-            'api_key': '',
+            'api_key': os.environ['gifAPI'],
             'q': query['query'],
             'limit': '1'
         }
